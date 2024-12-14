82ae5c23fa0ea9393f85530670ee3a5d938f1df2
Role1776 <197003zzz197003zzz@gmail.com>
2024-12-15 00:08:54 +0300

    goLama 2.0 update payload.go

--- lama/payload.go
+++ lama/payload.go
@@ -10,17 +10,18 @@ const (
 	defaultTimeout = 100 * time.Second
 )
 
-func sendRequest(url string, payloadJSON []byte) (*http.Response, error) {
+func sendRequest(url string, payloadJSON []byte, timeout time.Duration) (*http.Response, error) {
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadJSON))
 	if err != nil {
 		return nil, err
 	}
 
 	req.Header.Set("Content-Type", "application/json")
-	
-	
+	if timeout == 0 {
+		timeout = defaultTimeout
+	}
 	client := &http.Client{
-		Timeout: defaultTimeout,
+		Timeout: timeout,
 	}
 
 	resp, err := client.Do(req)
@@ -29,4 +30,5 @@ func sendRequest(url string, payloadJSON []byte) (*http.Response, error) {
 	}
 
 	return resp, nil
+
 }
